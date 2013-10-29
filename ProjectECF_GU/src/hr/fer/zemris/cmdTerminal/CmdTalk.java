dfcde1901ce323e5151ed14ae295590fb265c7c8
Truba <v.poluta@hotmail.com>
2013-10-29 18:58:15 +0100

    Promjenio da se cmd ne pojavi na ekranu prilikom poziva.

--- ProjectECF_GU/src/hr/fer/zemris/cmdTerminal/CmdTalk.java
+++ ProjectECF_GU/src/hr/fer/zemris/cmdTerminal/CmdTalk.java
@@ -7,10 +7,18 @@ public class CmdTalk {
 	public static void write(String path, String command){
 		Runtime rt = Runtime.getRuntime();
 		try {
+			//MY TESTING
 			//rt.exec("cmd.exe /c start cmd");
 			//rt.exec(new String[]{"cmd", "/k", "start", "cmd"});
 			//rt.exec("cmd.exe /c cd \""+new_dir+"\" & start cmd.exe /k \"java -flag -flag -cp terminal-based-program.jar\"");
-			rt.exec("cmd.exe /c cd \""+path+"\" & start cmd.exe /k \""+command+"\"");
+			
+			//IF CMD IS TO BEE SEEN:
+			//rt.exec("cmd.exe /c cd \""+path+"\" & start cmd.exe /k \""+command+"\"");
+			
+			//IF CMD IS NOT TO BEE SEEN:
+			rt.exec("cmd.exe /c cd \""+path+"\" & cmd.exe /k \""+command+"\"");
+			
+			
 		} catch (IOException e) {
 			System.err.println("Problem with writing to cmd.");
 			e.printStackTrace();
