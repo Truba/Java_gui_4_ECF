15b571e23ae0f02b8cd20b71f5eec0a56c261d36
mister11 <sven.vidak@fer.hr>
2013-11-01 00:48:14 +0100

    java after c continuation

--- ProjectECF_GU/src/hr/fer/zemris/cmdTerminal/Test.java
+++ ProjectECF_GU/src/hr/fer/zemris/cmdTerminal/Test.java
@@ -1,11 +1,16 @@
 package hr.fer.zemris.cmdTerminal;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Test {
 
 	/**
 	 * @param args
+	 * @throws FileNotFoundException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		DetectOS os = new DetectOS();
 		int osName = os.getOS();
 		System.out.println(osName);
@@ -17,9 +22,14 @@ public class Test {
 		if(osName == DetectOS.LINUX){
 			cmd = new TerminalTalk();
 		}
-		//cmd.write("lib","java -jar mythsim-3.1.1.jar");
-		cmd.write("C:", "C:/Users/Vlaho/Desktop/mythsim-3.1.1", "java -jar mythsim-3.1.1.jar");
-
+		cmd.write("lib","c.exe>in.txt");
+		File f = new File("lib/in.txt");
+		if(f.exists()) {
+			Scanner s = new Scanner(f);
+			System.out.println(s.hasNextLine() ? s.nextLine() : "Fatal error. exe did not write anything to file.");
+		}
+		//cmd.write("C:", "C:/Users/Vlaho/Desktop/mythsim-3.1.1", "java -jar mythsim-3.1.1.jar");
+		
 	}
 
 }
