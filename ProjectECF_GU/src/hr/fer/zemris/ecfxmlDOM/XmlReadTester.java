8d3ec65376f19bf032de1ad6ee1fc9be83fb86fc
Truba <v.poluta@hotmail.com>
2013-11-01 17:37:21 +0100

    Solozio slaganje novih xml-a nakon dobijenih parametara. Jedino glupi problem je sto se sve printa u jednom redu.

--- ProjectECF_GU/src/hr/fer/zemris/ecfxmlDOM/XmlReadTester.java
+++ ProjectECF_GU/src/hr/fer/zemris/ecfxmlDOM/XmlReadTester.java
@@ -1,5 +1,6 @@
 package hr.fer.zemris.ecfxmlDOM;
 
+import hr.fer.zemris.parameters.AlgGenReg4Writing;
 import hr.fer.zemris.parameters.AlgGenRegList;
 
 public class XmlReadTester {
@@ -9,8 +10,13 @@ public class XmlReadTester {
 	 */
 	public static void main(String[] args) {
 		
-		
+		//reading
 		AlgGenRegList sTANKOVIC_NAPRAVI_NESTO_S_OVIM =StartingXmlReading.read("lib/parameters.txt");
+		
+		//creating xml and writing in it
+		AlgGenRegList agr = sTANKOVIC_NAPRAVI_NESTO_S_OVIM;
+		AlgGenReg4Writing agrw = new AlgGenReg4Writing(agr.algorithms.get(1), agr.genotypes, agr.registry);
+		ParamsXmlWriting.write("lib/out.xml",agrw);
 
 	}
 
