4d120a244be8142bd42ea51fd05dd1d4c554425f
mister11 <sven.vidak@fer.hr>
2013-12-13 23:06:02 +0100

    done with testing concurrent running of algorithms

--- ProjectECF_GU/src/hr/fer/zemris/ecf/tasks/Test.java
+++ ProjectECF_GU/src/hr/fer/zemris/ecf/tasks/Test.java
@@ -7,12 +7,15 @@ import java.util.ArrayList;
 public class Test {
 	
 	public static void main(String[] args) {
-		String ecfPath = ""; //even unesi nesto
+		String ecfPath = "D:\\Documents\\SVEN\\FER\\Programiranje\\ECF_1.3\\debug\\examples\\GAonemax\\VS\\Debug\\gaonemax.exe";
 		String paramsPath = "lib\\parameters.xml";
 		
 		ArrayList<Job> jobs = new ArrayList<>();
-		for(int i=0; i<5; i++){
-			jobs.add(new Job(ecfPath, "lib/log"+i, paramsPath));
+		for(int i=0; i<3; i++){
+			jobs.add(new Job(ecfPath, "lib/log"+i+".txt", paramsPath));
+		}
+		for(int i=3; i<5; i++){
+			jobs.add(new Job(ecfPath, "lib/log"+i+".txt", "lib\\parameters1.txt"));
 		}
 		
 		TaskMannager ts = new TaskMannager();
