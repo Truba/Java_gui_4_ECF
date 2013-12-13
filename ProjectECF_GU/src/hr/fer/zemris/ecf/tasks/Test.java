8b318a516c2d275b6db29185e2c5b8e5dd1e7df1
mister11 <sven.vidak@fer.hr>
2013-12-13 23:16:51 +0100

    added testssss

--- ProjectECF_GU/src/hr/fer/zemris/ecf/tasks/Test.java
+++ ProjectECF_GU/src/hr/fer/zemris/ecf/tasks/Test.java
@@ -8,7 +8,7 @@ public class Test {
 	
 	public static void main(String[] args) {
 		String ecfPath = "D:\\Documents\\SVEN\\FER\\Programiranje\\ECF_1.3\\debug\\examples\\GAonemax\\VS\\Debug\\gaonemax.exe";
-		String paramsPath = "lib\\parameters.xml";
+		String paramsPath = "lib\\parameters.txt";
 		
 		ArrayList<Job> jobs = new ArrayList<>();
 		for(int i=0; i<3; i++){
