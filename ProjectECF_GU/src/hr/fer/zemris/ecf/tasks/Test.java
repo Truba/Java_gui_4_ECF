d6540e33eb2bb97a84d079a354aef5cffbf6b24d
mister11 <sven.vidak@fer.hr>
2013-12-17 18:27:11 +0100

    added two-digit print for one-digit numbers in log file name

--- ProjectECF_GU/src/hr/fer/zemris/ecf/tasks/Test.java
+++ ProjectECF_GU/src/hr/fer/zemris/ecf/tasks/Test.java
@@ -12,14 +12,21 @@ public class Test {
 		
 		ArrayList<Job> jobs = new ArrayList<>();
 		for(int i=0; i<3; i++){
-			jobs.add(new Job(ecfPath, "lib/log"+i+".txt", paramsPath));
+			jobs.add(new Job(ecfPath, "lib/log"+getNumOfJob(i)+".txt", paramsPath));
 		}
 		for(int i=3; i<5; i++){
-			jobs.add(new Job(ecfPath, "lib/log"+i+".txt", "lib\\parameters1.txt"));
+			jobs.add(new Job(ecfPath, "lib/log"+getNumOfJob(i)+".txt", "lib\\parameters1.txt"));
 		}
 		
 		TaskMannager ts = new TaskMannager();
 		ts.startTasks(jobs, ts.getCpuCors());
+	}
+
+	private static String getNumOfJob(int i) {
+		if(i < 10) {
+			return "0" + String.valueOf(i + 1);
+		} 
+		return String.valueOf(i + 1);
 	}
 
 }
